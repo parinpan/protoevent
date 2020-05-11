202a10df20c052cccc90695254b175ec7a885187
Fachrin Aulia <fachrinfan@gmail.com>
2020-05-11 21:29:14 +0700

    change listen strategy #2

--- protoevent.go
+++ protoevent.go
@@ -1,14 +1,13 @@
 package protoevent
 
 func CreateServant(network, address string) (Servant, ServerEvent, error) {
-	var err error
-	var listener = newListener(network, address)
+	listener := newListener(network, address)
 
-	if err = listener.Listen(); nil != err {
+	if err := listener.Listen(); nil != err {
 		return nil, nil, err
 	}
 
-	return newServant(listener), listener.GetEvent(), err
+	return newServant(listener), listener.GetEvent(), nil
 }
 
 func CreateAgent(network, address string) (Agent, ClientEvent) {
